e239a3603a37ccc7ef0499d8766115c2e1586cd2
Joaquin Blanco <blancojoaquin77@gmail.com>
2023-02-04 02:48:50 -0300

    refactor in scrape clarin

--- src/scrape.py
+++ src/scrape.py
@@ -7,6 +7,6 @@ def scrape_clarin() -> list[str]:
     clarin_html = get(url=url_clarin)
     clarin_web = BeautifulSoup(markup=clarin_html.text, features='html.parser')
     articulos = clarin_web.find_all(name='article')
-    titulos = [articulo.find('h2') for articulo in articulos]
+    titulos = [articulo.find(name='h2') for articulo in articulos]
     titulos = [titulo.text for titulo in titulos]
     return titulos
