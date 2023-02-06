28d08aabf7fb672bfbd2fdd0888b54b145be061a
Joaquin Blanco <blancojoaquin77@gmail.com>
2023-02-05 22:27:58 -0300

    debug mode implemented

--- src/main.py
+++ src/main.py
@@ -3,6 +3,6 @@ from scraper import ScrapeNews
 from router import RouterFlask
 from app import App
 
-app = App(scraper=ScrapeNews(), router=RouterFlask())
 mode_dev = len(argv) == 2 and argv[1] == 'dev'
+app = App(scraper=ScrapeNews(), router=RouterFlask())
 app.run(debugmode=mode_dev)
